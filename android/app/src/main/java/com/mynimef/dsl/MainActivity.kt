7b2038c3e1b9ae23edcbc50b6c2a23f884404c18
MYnimef <ivan.markov.2013@gmsil.com>
2022-08-05 22:42:13 +0300

    updated jni

--- android/app/src/main/java/com/mynimef/dsl/MainActivity.kt
+++ android/app/src/main/java/com/mynimef/dsl/MainActivity.kt
@@ -2,10 +2,33 @@ package com.mynimef.dsl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.mynimef.dsl.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'dsl' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'dsl' library on application startup.
+        init {
+            System.loadLibrary("dsl")
+        }
     }
 }
